cd2e6a84a663c74a3ec961e9fe09f9f63d71905c
chell-rui <chell_uoxou@yahoo.co.jp>
2016-09-19 10:50:30 +0900

    onCommandメソッドがないのにコマンドを追加しようとするとめっちゃ怒られるようにしました。

--- src/PHPPO/command/PluginCommands.php
+++ src/PHPPO/command/PluginCommands.php
@@ -14,11 +14,17 @@ $plugincommands = array();
  		global $plugincommands;
 		global $plugindata;
 		global $translators;
+		global $plugindata;
 		$translator = $translators->get('PO.System.Plugin.Package');
 		$enabled_message = $translator->translate('commandEnabled');
 		$plugin_name = $plugindata[$plugin]["name"];
 		global $defaultcommands;
- 		$plugincommands[$basecommand] = array('pluginname'=>$plugin_name,'basecommand'=>$basecommand,'type' => $type, 'des' => $des, 'usage' => $usage, 'enadis'=> $enadis);
-		$this->info("\x1b[38;5;227m[{$plugin}] \x1b[38;5;83m{$enabled_message}\x1b[38;5;231m:\x1b[38;5;145m{$basecommand}");
+		$obj = new $plugindata[$plugin]["main"];
+		if (method_exists($obj,"onCommand")) {
+			$plugincommands[$basecommand] = array('pluginname'=>$plugin_name,'basecommand'=>$basecommand,'type' => $type, 'des' => $des, 'usage' => $usage, 'enadis'=> $enadis);
+			$this->info("\x1b[38;5;227m[{$plugin}] \x1b[38;5;83m{$enabled_message}\x1b[38;5;231m:\x1b[38;5;145m{$basecommand}");
+		}else{
+			$this->throwError('Couldn\'t add to the command (' . $basecommand . '). Because "onCommand" method not found in the plugin processing.' , "critical");
  	}
  }
+}
