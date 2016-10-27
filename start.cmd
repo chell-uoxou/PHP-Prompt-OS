ae3c8e29cf6bbd2e5bde0b8e1ef92e4c4ed73984
chell-rui <chell_uoxou@yahoo.co.jp>
2016-10-27 16:51:53 +0900

    起動コマンド簡潔化

--- start.cmd
+++ start.cmd
@@ -2,7 +2,7 @@
 TITLE PHP Prompt OS launcher
 cd /d %~dp04
 if exist bin\php7\php.exe (
-	start bin\mintty.exe -o Columns=88 -o Rows=32 -o AllowBlinking=0 -o FontQuality=10 -o Font="Lucida Sans Typewriter" -o FontHeight=11 -o CursorType=0 -o CursorBlinks=1 -h error -t "PHP Prompt OS" -w max /bin/php7/php.exe src/PHPPO/PHPPO.php --with-readline %*
+	start bin\mintty.exe -o Columns=88 -o Rows=32 -o AllowBlinking=0 -o FontQuality=10 -o Font="Lucida Sans Typewriter" -o FontHeight=11 -o CursorType=0 -o CursorBlinks=1 -h error -t "PHP Prompt OS" -w max /bin/php7/php.exe src/PHPPO/PHPPO.php %*
 ) else (
 	echo Did you install "PHP-binary"?
 	echo You must download it from https://github.com/chell-uoxou/PHP-Binaries and put on "bin" directory it.
